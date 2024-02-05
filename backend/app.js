ab6d578170a1035b9c2e4db72c6aa67e7a4e0562
Kizito007 <nwakakelechi49@gmail.com>
2024-02-05 23:05:40 +0100

    added cors all

--- backend/app.js
+++ backend/app.js
@@ -8,7 +8,7 @@ const path = require("path");
 const fileUpload = require('express-fileupload');
 
 app.use(cors({
-  origin: ['https://allsextoys.vercel.app'],
+  origin: '*',
   credentials: true
 }));
 // app.use(express.bodyParser({limit: '50mb'}))
