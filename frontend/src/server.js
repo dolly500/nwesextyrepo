a7da3fda6e62420eaf4d62268d97b5657e40ce81
Kizito007 <nwakakelechi49@gmail.com>
2024-02-07 00:01:16 +0100

    changed server

--- frontend/src/server.js
+++ frontend/src/server.js
@@ -1,4 +1,4 @@
-export const server = "https://lucky-lamb-kit.cyclic.app/api/v2";
+export const server = "https://allsextoys-backend.onrender.com/api/v2";
 
 // export const server = "http://localhost:8000/api/v2";
 
