b40142b2c0c776e19d09b57fcaf30815989cba35
“dolly500” <dolapoakamo01@gmail.com>
2024-07-06 19:29:27 +0100

    Your commit message

--- frontend/src/server.js
+++ frontend/src/server.js
@@ -1,5 +1,5 @@
-// export const server = "https://nwesextyrepo-apis.onrender.com/api/v2";
+export const server = "https://nwesextyrepo-apis.onrender.com/api/v2";
 
-export const server = "http://localhost:4000/api/v2";
+
 
 
